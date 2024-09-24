de0378f2aedb48dea11c8d51402bbadc52b4e3b0
Jesús Mur Fontanals <murfontanals@gmail.com>
2024-09-24 12:57:28 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin-ext"],
+});
+
+export const metadata: Metadata = {
+  title: "Device Shop",
+  description: "Project for practicing Tailwind CSS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
